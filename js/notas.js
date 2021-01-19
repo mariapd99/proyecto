5cb49237b97e0e7862667e70dd5673038d84dd34
mariapd99 <maria_perez99@live.com>
2021-01-19 21:57:59 +0100

    logo contacto + notas

--- js/notas.js
+++ js/notas.js
@@ -1,26 +1,23 @@
-/*$(document).ready(function() {
-	$.getJSON("http://localhost:8081/alumnos/12345678W",function(listaNotas){
-		$.each(listaNotas, function(index, nota){
-			var notaHTML = "<div><p><strong>"+nota.acronimo+"</strong></p><p>Nota final: "+nota.nota+"</p></div>";
-			$("#notas").append(notaHTML);
-		})
-	});
-});*/
-
 $(document).ready(function() {
 
     $.ajax({ 
-        type: "GET", 
+        type: "GET",
         url: "http://localhost:8081/alumnos/12345678W",
+        headers: {
+            Accept: "application/json",
+        },
         dataType: "json",
         success: function(listaNotas) {
-            //$.each(listaNotas, function(index, nota){
-                var notaHTML = "<p>su puta madre</p>";
+            $.each(listaNotas, function(index, nota){
+                console.log(listaNotas);
+                var notaHTML = "<div><p><strong>"+nota.acronimo+"</strong></p><p>Nota final: "+nota.nota+"</p></div>";
                 $("#notas").append(notaHTML);
-            //})
-        }
+            })
+        },
+        error: (jqXhr, textStatus, errorMessage)=>{
+            console.error(errorMessage);
+            console.error(jqXhr);
+            console.error(textStatus);
+            }
     });
 });
-
-
-//var notaHTML = "<div><p><strong>"+nota.acronimo+"</strong></p><p>Nota final: "+nota.nota+"</p></div>";
